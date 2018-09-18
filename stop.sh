ef244a23b4a706f97f37dd61df93357905c14098
danilodeLuca <danilo.luca@dextra-sw.com>
2018-09-18 15:55:21 -0300

    readme

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 #Remove containers created earlier
-docker stop -f ifood/backend-danilo
+docker stop ifood/backend-danilo
 
 docker-compose -f env_setup/docker-compose.yml stop
 docker-compose -f env_setup/docker-compose.yml rm -f
