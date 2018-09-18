2f34d65384ffcba07f6b519d379f6c224bf69c73
danilodeLuca <danilo.luca@dextra-sw.com>
2018-09-18 15:59:12 -0300

    readme

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 #Remove containers created earlier
-docker stop ifood/backend-danilo
+docker stop ifood/backend-danilo || true
 
 docker-compose -f env_setup/docker-compose.yml stop
 docker-compose -f env_setup/docker-compose.yml rm -f
 
-docker rmi -f ifood/backend-danilo
+docker rmi -f ifood/backend-danilo || true
 
 ## build projeto
 mvn clean install
